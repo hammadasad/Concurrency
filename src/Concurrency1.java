03eea873b72ee8624f950c976f7ea8d68363015b
Hammad Asad <hammad_asad@msn.com>
2018-04-23 17:20:41 -0400

    Thread Joins

---
+++ src/Concurrency1.java
@@ -0,0 +1,41 @@
+class Runner1 implements Runnable {
+    public void startRunning() {
+
+    }
+
+    public void run() {
+        for(int i = 0 ; i < 10 ; ++i) {
+            System.out.println("Runner1 " + i);
+        }
+    }
+}
+
+class Runner2 implements Runnable {
+
+    public void run() {
+        for(int i = 0 ; i < 10 ; ++i) {
+            System.out.println("Runner2 " + i);
+        }
+    }
+}
+
+public class Concurrency1 {
+
+    public static void main(String[] args) {
+        Thread t1 = new Thread(new Runner1());
+        Thread t2 = new Thread(new Runner2());
+
+        t1.start();
+        t2.start();
+
+
+        try {
+            t1.join();
+            t2.join();
+        } catch(InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Finished the tasks");
+    }
+}
